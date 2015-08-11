2a76a7f68e5370f547d0735013d0998027cedc1a
Alex Eagle <alexeagle@google.com>
2015-08-11 12:35:18 -0700

    example

---
+++ typings/tsd.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="fs-extra/fs-extra.d.ts" />
+/// <reference path="minimist/minimist.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="source-map-support/source-map-support.d.ts" />
+/// <reference path="source-map/source-map.d.ts" />
